7395f3cc292f28303dcef60cbbd8a8defeb72e9e
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-07-28 08:43:35 -0700

    added startup.cs controller folder and OAuth file. Getting errors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Autodesk.Forge.Core;
+using Autodesk.Forge.DesignAutomation;
+using Microsoft.AspNetCore;
 
-app.Run();
+namespace designAutomationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"appsettings.user.json", optional: true);
+                builder.AddEnvironmentVariables();
+
+            }).ConfigureServices((hostContext, services) =>
+            {
+                services.AddDesignAutomation(hostContext.Configuration);
+            }).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
